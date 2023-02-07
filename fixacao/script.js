b677bb03485c4b75aa8652fc697593158e0437c3
gustaMACHO <henrique.gusta777@gmail.com>
2023-02-06 21:47:00 -0300

    exercicios-fixacao

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,22 @@
-// Resolva o Exercício de Fixação!
+// receba um nome, um ano de nascimento e o ano atual
+
+function verificarMaioridade(nome, anoDeNascimento, anoAtual) {
+    const idade = (anoAtual - anoDeNascimento) >= 18
+ 
+
+
+    const frase = (`${nome} é maior de idade? ${ehmaoir}`)
+    return frase 
+}
+
+
+const pessoa1 = verificarMaioridade('gustavo', 2002, 2023)
+//console.log(pessoa1)
+
+
+const pessoa2 = verificarMaioridade('maria', 2002, 2023)
+//console.log(pessoa2)
+
+
+const pessoa3 = verificarMaioridade('mariah', 2018, 2023)
+//console.log(pessoa3)
