bf6a0d7a665bc0a71cd1f9dca5e9d1e0f411edd8
DavidBrito32 <davidbrito.carneiro458@gmail.com>
2023-05-08 20:57:16 -0300

    Resolucao dos exercicios

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,12 @@
 // Resolva o Exercício de Fixação!
+
+const comparacao = (nome, anoNasc, anoAtual) => {
+
+    return `Ola, ${nome}, voce ja é maior de idade? ${anoAtual - anoNasc >= 18}!, Voce tem: ${anoAtual - anoNasc} Anos de idade!`;
+}
+
+console.log(comparacao("david", 1996, 2023));
+console.log(comparacao("Pedro Gabriel", 2010, 2023));
+console.log(comparacao("Iudi laurindo", 2009, 2023));
+
+
