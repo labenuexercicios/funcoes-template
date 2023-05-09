bde2790d05d106bec9f73db31416a9e65ea07ec0
Wallacy <wallacy_vinicius@hotmail.com>
2023-05-08 23:02:34 -0300

    Exercício de Funções 1

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,26 @@
 // Resolva o Exercício de Fixação!
+
+const verificacao = (nome, anoDeNascimento, anoAtual) => {
+    let idade = anoDeNascimento - anoAtual
+    let maiorDe18 = anoAtual - anoDeNascimento >= 18
+
+    return `${nome} é maior de idade? ${maiorDe18}.`
+}
+    
+
+
+console.log(verificacao(`Wallacy`, 1999, 2023));
+
+
+// function verificacao(nome, anoDeNascimento, anoAtual) {
+//     let idade = anoDeNascimento - anoAtual
+//     let maiorDe18 = anoAtual - anoDeNascimento >= 18
+
+//     return `${nome} é maior de idade? ${maiorDe18}.`
+// }
+    
+
+
+// console.log(verificacao(`Wallacy`, 1999, 2023));
+
+
--- pratica-guiada/script.js
+++ pratica-guiada/script.js
@@ -1,1 +1,38 @@
 // Acompanhe a Prática Guiada e repita o exercício no Momento de Réplica!
+
+// function imprimirNome(nome){
+        //bloco de código
+    // console.log(`Olá ${nome}`);
+//}
+
+const imprimirNome = function (nome){
+    console.log(`Olá, ${nome}`);
+}
+
+imprimirNome(`Wallacy`)
+imprimirNome("Tito")
+imprimirNome("Zeus")
+
+// ---------------------------------------
+const fazerOperacoes = (numero) => {
+    const verifica = numero % 2 === 0
+    const soma = numero + 10
+    const multiplicacao = numero * numero
+
+        return (`O número ${numero} é par ${verifica}. Somado com 10 o resultado é ${soma}. Multiplicado por ele mesmo o resiltado ${multiplicacao}`)
+}
+
+console.log(fazerOperacoes(12));
+
+//------------------------------------------
+const verificaDados = (login, senha) => {
+    const loginArmazenado = "teste@gmail.com"
+    const senhaArmazenada = "123456"
+
+    return loginArmazenado === login && senhaArmazenada === senha
+
+}
+
+console.log(verificaDados("teste@gmail.com", "123456"));
+
+// chama sempre a primeira CONST console.log(*nome*(depois do =))
