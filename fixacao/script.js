995090f02e90a884d5bfd00e20dc1e58ccaf1e8a
Douglasluis508 <douglasluis508@gmail.com>
2023-05-09 18:35:01 -0300

    pratica guiada

--- fixacao/script.js
+++ fixacao/script.js
@@ -1,1 +1,17 @@
-// Resolva o Exercício de Fixação!
+// Resolva o Exercício de Fixação
+// function verificarIdade (nome, anonascimento, anoatual) { 
+//     let idade = anoatual - anonascimento
+//     let maiorIdade = idade >= 18
+//     return (`${nome} é maior de idade? ${maiorIdade}`)
+
+
+// }
+// console.log(verificarIdade('Douglas', 1991, 2023))
+
+const verificaoIdade = (nome, anoNasci, anoAtual) =>{
+    let idade = anoAtual - anoNasci
+    let maioIdade = idade >= 18
+    return `${nome} é maior de idade?${maioIdade}`
+
+}
+console.log(verificaoIdade('Douglas', 1991, 2023))
--- pratica-guiada/script.js
+++ pratica-guiada/script.js
@@ -1,1 +1,30 @@
-// Acompanhe a Prática Guiada e repita o exercício no Momento de Réplica!
+const nome = (nome)=>{ 
+    return `Olá ${nome}`
+}
+
+console.log(nome('Douglas'))
+console.log(nome('pipoca'))
+console.log(nome('Lenara'))
+
+
+
+
+
+
+const numerPar = (numero) =>{ 
+    let verifica = numero % 2 === 0
+    let soma = numero + 10
+    let multiplica = soma + soma
+    return `O número ${numero} é par? ${verifica}.Somado com 10, o resultado é ${soma} e multiplicado por ele mesmo o resultado é ${multiplica}`
+}
+console.log(numerPar(12))
+
+
+
+const loginEsenha = (login, senha)=>{ 
+    let loginArmazenado = 'Douglasluis508'
+    let senhaArmazenada = '8647251086'
+    return loginArmazenado === login && senhaArmazenada === senha
+
+}
+console.log (loginEsenha('Douglasluis508', '8647251086'))
